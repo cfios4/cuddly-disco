414fbef1e09efd2c573a7c41e0e69fadec5692c0
casey <casey@cafio.co>
2025-01-31 22:46:49 -0800

    quotes

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 
 provider "proxmox" {
   pm_tls_insecure = true # By default Proxmox Virtual Environment uses self-signed certificates.
-  pm_api_url = ${var.pm_api_url}
-  pm_api_token_id = ${var.pm_api_token_id}
-  pm_api_token_secret = ${var.pm_api_token_secret}
+  pm_api_url = "${var.pm_api_url}"
+  pm_api_token_id = "${var.pm_api_token_id}"
+  pm_api_token_secret = "${var.pm_api_token_secret}"
 }
 
 resource "proxmox_vm_qemu" "create-talos" {
