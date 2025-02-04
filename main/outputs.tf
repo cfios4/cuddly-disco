3589bc7da6a5cdfb2975e24f2ba6911267a7bdfc
casey <casey@cafio.co>
2025-02-03 19:44:54 -0800

    update syntax

--- main/outputs.tf
+++ main/outputs.tf
@@ -1,17 +1,17 @@
 output "control_plane_ips" {
   description = "IP addresses of the control plane nodes"
-  value       = [for i in range(local.control_plane_nodes) : proxmox_virtual_environment_vm.control_plane[i].ipconfig0]
+  value       = [for i in range(local.control_plane_nodes) : proxmox_virtual_environment_vm.control_plane[i].ipv4_addresses]
 }
 
 output "worker_ips" {
   description = "IP addresses of the worker nodes"
-  value       = [for i in range(local.worker_nodes) : proxmox_virtual_environment_vm.worker[i].ipconfig0]
+  value       = [for i in range(local.worker_nodes) : proxmox_virtual_environment_vm.worker[i].ipv4_addresses]
 }
 
 output "all_node_ips" {
   description = "All node IP addresses"
   value       = concat(
-    [for i in range(local.control_plane_nodes) : proxmox_virtual_environment_vm.control_plane[i].ipconfig0],
-    [for i in range(local.worker_nodes) : proxmox_virtual_environment_vm.worker[i].ipconfig0]
+    [for i in range(local.control_plane_nodes) : proxmox_virtual_environment_vm.control_plane[i].ipv4_addresses],
+    [for i in range(local.worker_nodes) : proxmox_virtual_environment_vm.worker[i].ipv4_addresses]
   )
 }
