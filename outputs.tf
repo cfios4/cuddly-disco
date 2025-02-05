fdc09813b7a10041f60d75acf1d5ee7ccd4c0acd
casey <casey@cafio.co>
2025-02-04 22:07:51 -0800

    wtf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "control_plane_ips" {
   description = "IP addresses of the control plane nodes"
-  value       = proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses[1]
+  value       = [for i in range(local.control_plane) : proxmox_virtual_environment_vm.control_plane[i].ipv4_addresses[1]]
 }
 
 output "worker_ips" {
