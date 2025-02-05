70339daefc4fde165ec7c283b84911438250a57a
casey <casey@cafio.co>
2025-02-04 21:53:01 -0800

    get right ips

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "control_plane_ips" {
   description = "IP addresses of the control plane nodes"
-  value       = proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses[*]
+  value       = proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses[1][0]
 }
 
 output "worker_ips" {
