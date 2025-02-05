7c6c5c283bbf05e6cc834c4b15bba101bf126869
casey <casey@cafio.co>
2025-02-04 21:34:48 -0800

    not done troubleshooting that issue

--- cluster.sh
+++ cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 curl -o /tmp/talosctl -L $(curl -s https://api.github.com/repos/siderolabs/talos/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4)
 chmod +x /tmp/talosctl
