ddd2af80db84bb1b07d2a4b72804d049cc7cfcc1
casey <casey@cafio.co>
2025-02-04 21:24:00 -0800

    permissions saving in /usr/local/bin

--- cluster.sh
+++ cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 
-curl -sL https://talos.dev/install | sh
+curl -o /usr/local/bin/talosctl -L $(curl -s https://api.github.com/repos/siderolabs/talos/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4)
+chmod +x /usr/local/bin/talosctl
 
 talosctl gen config t3s https://${CNODES[0]} --install-image factory.talos.dev/installer/6bd594353a7860f79cc0910931af93ffd6890ef32aaa01db81eb90c1de2e55e6:v1.9.3 --config-patch-control-plane '{"cluster": {"allowSchedulingOnControlPlanes": true}}'
 
