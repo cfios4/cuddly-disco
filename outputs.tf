a0c30d08169911c615b2aeaa85c4f38561f17a1e
casey <casey@cafio.co>
2025-02-05 19:52:14 -0800

    try other proxmox provider

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
-// output "control_plane_ips" {
-//   description = "IP addresses of the control plane nodes"
-//   value       = [for i in range(local.control_plane_nodes) : default_ipv4_address.control_plane[i].ipv4_addresses]
-// }
+output "control_plane_ips" {
+  description = "IP addresses of the control plane nodes"
+  value       = [for i in range(local.control_plane_nodes) : default_ipv4_address.control_plane[i].ipv4_addresses]
+}
 
-// output "worker_ips" {
-//   description = "IP addresses of the worker nodes"
-//   value       = [for i in range(local.worker_nodes) : default_ipv4_address.worker[i].ipv4_addresses]
-// }
+output "worker_ips" {
+  description = "IP addresses of the worker nodes"
+  value       = [for i in range(local.worker_nodes) : default_ipv4_address.worker[i].ipv4_addresses]
+}
 
-// output "all_node_ips" {
-//   description = "All node IP addresses"
-//   value       = concat(
-//     [for i in range(local.control_plane_nodes) : default_ipv4_address.control_plane[i].ipv4_addresses],
-//     [for i in range(local.worker_nodes) : default_ipv4_address.worker[i].ipv4_addresses]
-//   )
-// }
+output "all_node_ips" {
+  description = "All node IP addresses"
+  value       = concat(
+    [for i in range(local.control_plane_nodes) : default_ipv4_address.control_plane[i].ipv4_addresses],
+    [for i in range(local.worker_nodes) : default_ipv4_address.worker[i].ipv4_addresses]
+  )
+}
