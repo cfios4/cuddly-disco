b7f6034c24a4378d1f733d484613473ba0b5d370
casey <casey@cafio.co>
2025-02-05 22:41:09 -0800

    update variables for arry?

--- cluster.sh
+++ cluster.sh
@@ -20,27 +20,27 @@ filter_loopbacks() {
 }
 
 # Filter out loopback IPs from CNODES and WNODES
-IFS=' ' read -r -a CNODES <<< filter_loopbacks "${CNODES[@]}"
-IFS=' ' read -r -a WNODES <<< filter_loopbacks "${WNODES[@]}"
+CONTROL=$(filter_loopbacks "${CNODES[@]}")
+WORK=$(filter_loopbacks "${WNODES[@]}")
 
 curl -o /tmp/talosctl -L $(curl -s https://api.github.com/repos/siderolabs/talos/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4)
 chmod +x /tmp/talosctl
 
-/tmp/talosctl gen config t3s https://${CNODES[0]} --install-image factory.talos.dev/installer/6bd594353a7860f79cc0910931af93ffd6890ef32aaa01db81eb90c1de2e55e6:v1.9.3 --config-patch-control-plane '{"cluster": {"allowSchedulingOnControlPlanes": true}}'
+/tmp/talosctl gen config t3s https://${CONTROL[0]} --install-image factory.talos.dev/installer/6bd594353a7860f79cc0910931af93ffd6890ef32aaa01db81eb90c1de2e55e6:v1.9.3 --config-patch-control-plane '{"cluster": {"allowSchedulingOnControlPlanes": true}}'
 
-for controller in ${CNODES[@]} ; do
+for controller in ${CONTROL[@]} ; do
     /tmp/talosctl apply-config -f controlplane.yaml -n $controller -e $controller --insecure
 done
 
-for worker in ${WNODES[@]} ; do
+for worker in ${WORK[@]} ; do
     /tmp/talosctl apply-config -f worker.yaml -n $worker -e $worker --insecure
 done
 
-until /tmp/talosctl bootstrap -n ${CNODES[0]} -e ${CNODES[0]} --talosconfig ./talosconfig ; do
+until /tmp/talosctl bootstrap -n ${CONTROL[0]} -e ${CONTROL[0]} --talosconfig ./talosconfig ; do
     echo "Node rebooting..."
     sleep 2
 done
 
-/tmp/talosctl kubeconfig -n ${CNODES[0]} -e ${CNODES[0]} --talosconfig ./talosconfig
+/tmp/talosctl kubeconfig -n ${CONTROL[0]} -e ${CONTROL[0]} --talosconfig ./talosconfig
 
 # cat ./.kube/config ; echo "----------------------------" ; cat ./talosconfig
