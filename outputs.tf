4f7e1a5a16437fa70e21ad491eaac9f919785eb5
casey <casey@cafio.co>
2025-02-05 21:30:08 -0800

    try other proxmox provider

--- outputs.tf
+++ outputs.tf
@@ -10,8 +10,8 @@ output "worker_ips" {
 
 output "all_node_ips" {
   description = "All node IP addresses"
-  value       = concat(
+  value       = [
     element(compact(flatten([for ip_list in proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses : ip_list])), 1),
     element(compact(flatten([for ip_list in proxmox_virtual_environment_vm.worker[*].ipv4_addresses : ip_list])), 1)
-  )
+  ]
 }
