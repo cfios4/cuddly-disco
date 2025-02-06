7efdd6a957722cf95008ed18fd8e66ac1ca3232d
casey <casey@cafio.co>
2025-02-05 19:50:51 -0800

    try other proxmox provider

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "proxmox" {
   pm_api_url  = "${var.endpoint}"
-  pm_api_token_id = "${var.api_id}"
-  pm_api_token_secret = "${var.api_secret}"
+  pm_api_token_id = "${var.pm_api_token_id}"
+  pm_api_token_secret = "${var.pm_api_token_secret}"
 
   pm_tls_insecure = true
 }
