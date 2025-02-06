a6e4a25dec37138a1e78770522804fed86464b2f
casey <casey@cafio.co>
2025-02-05 21:15:12 -0800

    try other proxmox provider

--- outputs.tf
+++ outputs.tf
@@ -18,18 +18,18 @@
 
 output "control_plane_ips" {
   description = "IP addresses of the control plane nodes"
-  value       = proxmox_virtual_environment_vm.control_plane.ipv4_addresses
+  value       = proxmox_virtual_environment_vm.control_plane.ipv4_addresses[1][0]
 }
 
 output "worker_ips" {
   description = "IP addresses of the worker nodes"
-  value       = proxmox_virtual_environment_vm.worker.ipv4_addresses
+  value       = proxmox_virtual_environment_vm.worker.ipv4_addresses[0]
 }
 
-output "all_node_ips" {
-  description = "All node IP addresses"
-  value       = concat(
-    proxmox_virtual_environment_vm.control_plane.ipv4_addresses,
-    proxmox_virtual_environment_vm.worker.ipv4_addresses
-  )
-}
+// output "all_node_ips" {
+//   description = "All node IP addresses"
+//   value       = concat(
+//     proxmox_virtual_environment_vm.control_plane.ipv4_addresses,
+//     proxmox_virtual_environment_vm.worker.ipv4_addresses
+//   )
+// }
