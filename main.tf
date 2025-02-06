6114da5276f2a63a65f7ce5153fc9892a925bf34
casey <casey@cafio.co>
2025-02-05 21:08:30 -0800

    try other proxmox provider

--- main.tf
+++ main.tf
@@ -13,56 +13,29 @@ provider "proxmox" {
   insecure = true
 }
 
-locals {
-  control_plane_nodes = ceil(var.total_nodes / 2) + (var.total_nodes % 2 == 0 ? 1 : 0)
-  worker_nodes        = var.total_nodes - local.control_plane_nodes
-}
+// locals {
+//   control_plane_nodes = ceil(var.total_nodes / 2) + (var.total_nodes % 2 == 0 ? 1 : 0)
+//   worker_nodes        = var.total_nodes - local.control_plane_nodes
+// }
 
 resource "proxmox_virtual_environment_vm" "control_plane" {
-  count = local.control_plane_nodes
-  name        = "talos-control-${count.index + 1}"
+  // count = local.control_plane_nodes
+  name        = "talos-control" #-${count.index + 1}"
   node_name = "prox"
-
-  cpu {
-    cores = 2
-    type = "x86-64-v2-AES"
-  }
-
-  memory {
-    dedicated = 2048
-  }
 
   agent {
     enabled = true
   }
-
-  operating_system {
-    type = "l26"
-  }
   
-  cdrom {
-    enabled   = true
-    file_id   = "local:iso/talos-nocloud-amd64.iso"
-    interface = "ide2"
-  }
-
-  disk {
-    interface = "scsi0"
-    size = 16
-    file_format = "raw"
-  }
-
-  network_device {
-    bridge = "vmbr0"
-    firewall = true
-    model = "virtio"
+  clone {
+    vm_id = "107"
   }
 
 }
 
 resource "proxmox_virtual_environment_vm" "worker" {
-  count = local.worker_nodes
-  name        = "talos-worker-${count.index + 1}"
+  // count = local.worker_nodes
+  name        = "talos-worker" #-${count.index + 1}"
   node_name = "prox"
 
   agent {
