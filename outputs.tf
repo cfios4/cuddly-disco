3b13214065e5b68b23ee00a7e010f91eb6e52c80
casey <casey@cafio.co>
2025-02-05 21:37:27 -0800

    try other proxmox provider

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,14 @@
 output "control_plane_ips" {
   description = "IP addresses of the control plane nodes"
-  value       = compact(flatten([for ip_list in proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses : ip_list]))
+  value       = [
+    compact(
+      flatten(
+        [
+          for ip_list in proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses : [for ip in ip_list : ip if ip != "127.0.0.1"]
+        ]
+      )
+    )
+  ]
 }
 
 output "worker_ips" {
