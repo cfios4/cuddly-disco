d6653d63f01d02e5cdba560cfa43b5c55a0d4d74
casey <casey@cafio.co>
2025-02-05 21:28:05 -0800

    try other proxmox provider

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ provider "proxmox" {
   insecure = true
 }
 
-// locals {
-//   control_plane_nodes = ceil(var.total_nodes / 2) + (var.total_nodes % 2 == 0 ? 1 : 0)
-//   worker_nodes        = var.total_nodes - local.control_plane_nodes
-// }
+locals {
+  control_plane_nodes = ceil(var.total_nodes / 2) + (var.total_nodes % 2 == 0 ? 1 : 0)
+  worker_nodes        = var.total_nodes - local.control_plane_nodes
+}
 
 resource "proxmox_virtual_environment_vm" "control_plane" {
-  // count = local.control_plane_nodes
-  name        = "talos-control" #-${count.index + 1}"
+  count = local.control_plane_nodes
+  name        = "talos-control-${count.index + 1}"
   node_name = "prox"
 
   agent {
@@ -34,8 +34,8 @@ resource "proxmox_virtual_environment_vm" "control_plane" {
 }
 
 resource "proxmox_virtual_environment_vm" "worker" {
-  // count = local.worker_nodes
-  name        = "talos-worker" #-${count.index + 1}"
+  count = local.worker_nodes
+  name        = "talos-worker-${count.index + 1}"
   node_name = "prox"
 
   agent {
--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
-// output "control_plane_ips" {
-//   description = "IP addresses of the control plane nodes"
-//   value       = [for i in range(local.control_plane_nodes) : proxmox_virtual_environment_vm.control_plane[i].ipv4_addresses]
-// }
+output "control_plane_ips" {
+  description = "IP addresses of the control plane nodes"
+  value       = element(compact(flatten([for ip_list in proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses : ip_list])), 1)
+}
 
 output "worker_ips" {
   description = "IP addresses of the worker nodes"
   value       = element(compact(flatten([for ip_list in proxmox_virtual_environment_vm.worker[*].ipv4_addresses : ip_list])), 1)
 }
 
-// output "all_node_ips" {
-//   description = "All node IP addresses"
-//   value       = concat(
-//     [for i in range(local.control_plane_nodes) : proxmox_virtual_environment_vm.control_plane[i].ipv4_addresses],
-//     [for i in range(local.worker_nodes) : proxmox_virtual_environment_vm.worker[i].ipv4_addresses]
-//   )
-// }
+output "all_node_ips" {
+  description = "All node IP addresses"
+  value       = concat(
+    element(compact(flatten([for ip_list in proxmox_virtual_environment_vm.control_plane[*].ipv4_addresses : ip_list])), 1),
+    element(compact(flatten([for ip_list in proxmox_virtual_environment_vm.worker[*].ipv4_addresses : ip_list])), 1)
+  )
+}
